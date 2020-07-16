a319525bc63c12c4302ec8cbfe2b347b6e546879
kathanzalek <66512090+kathanzalek@users.noreply.github.com>
2020-07-16 16:28:03 +1000

    Add files via upload
    gender reveal. 
    
    Baby was meant to dance.. but couldn't get it to work.

---
+++ main.js
@@ -0,0 +1,47 @@
+//blue box
+
+// step 1: select blue box
+var lightblueBox = document.querySelector('.lightblue')
+
+// step 2: listen for it to be clicked
+lightblueBox.addEventListener('click', function () {
+
+// step 3: once clicked, change background
+var body = document.querySelector('body')
+body.style.background = 'lightblue'
+var h1 = document.querySelector('h1')
+h1.style.color = "black"
+blackbox.style.border = "black 1px solid"
+})
+
+//pink box
+
+// step 1: select whote box
+var hotpinkBox = document.querySelector('.hotpink')
+
+// step 2: listen for it to be clicked
+hotpinkBox.addEventListener('click', function () {
+
+  // step 3: once clicked, change background
+  var body = document.querySelector('body')
+  body.style.background = 'hotpink'
+  var h1 = document.querySelector('h1')
+  h1.style.color = "black"
+  blackbox.style.border = "black 1px solid"
+})
+
+//green box
+
+// step 1: select whote box
+var greenyellow = document.querySelector('.greenyellow')
+
+// step 2: listen for it to be clicked
+greenyellow.addEventListener('click', function () {
+
+  // step 3: once clicked, change background
+  var body = document.querySelector('body')
+  body.style.background = 'greenyellow'
+  var h1 = document.querySelector('h1')
+  h1.style.color = "black"
+  blackbox.style.border = "black 1px solid"
+})
